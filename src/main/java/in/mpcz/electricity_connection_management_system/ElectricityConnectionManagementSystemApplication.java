6bfb572ae1ea2a580294e65183e8b3567de78b25
rajpoot25m@gmail.com <rajpoot25m@gmail.com>
2026-07-24 15:38:09 +0530

    Created initial project structure following layered architecture

---
+++ src/main/java/in/mpcz/electricity_connection_management_system/ElectricityConnectionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package in.mpcz.electricity_connection_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityConnectionManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityConnectionManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/mpcz/electricity_connection_management_system/ElectricityConnectionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mpcz.electricity_connection_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityConnectionManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
